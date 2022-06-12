eaeed710e49eb12668dd1c55fbc8840e00a64e16
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-06-12 23:44:31 +0300

    first commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,44 @@
+<template>
+ <v-footer id="social" dark padless>
+    <v-card class="black white--text py-5 px-5 text-center" width="100%" flat tile >
+        <v-row>
+            <v-col cols="12" sm="4">
+                <v-card-text class="white--text pt2">
+                    <v-btn icon>
+                        <v-icon color="orange accent-4"> apple</v-icon>
+                    </v-btn>
+                </v-card-text>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <v-card-text class="pt-2">
+                    <v-btn class="mx-1 white--text" icon v-for="icon in icons" :key="icon">
+                        <v-icon size="24px">{{icon}}</v-icon>
+                    </v-btn>
+                </v-card-text>
+            </v-col>
+            <v-col cols="12" sm="4">
+                <v-card-text class="pt-2">
+                    <v-btn class="mx-1 white--text" icon>
+                        <v-icon color="orange accent-4">mdi-heart</v-icon>
+                    </v-btn>
+                    Created by MudWebDev
+                </v-card-text>
+            </v-col>
+        </v-row>
+    </v-card>
+ </v-footer>
+</template>
+
+<script>
+export default {
+data: () => ({
+    icons: [
+        "mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"
+    ]
+})
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app-bar app color="#0c0c0d" dark flat class="px-8">
+        <v-btn icon>
+            <v-icon color="white"> apple</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text @click="scroll('the-home')">iPhone</v-btn>
+        <v-btn text @click="scroll('mac')">Mac</v-btn>
+        <v-btn text @click="scroll('airpods')">Airpods</v-btn>
+        <v-btn text @click="scroll('contact')">Contact</v-btn>
+        <v-btn text @click="scroll('social')">Social</v-btn>
+        
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    methods: {
+        scroll(refName) {
+            const element = document.getElementById(refName);
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+}
+</script>
+
+<style>
+</style>
